e4ca8ff8cebbba8c82c4d1939a1980d56cd59818
Richard <richardjalba@gmail.com>
2020-09-15 05:37:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const lodash = require("lodash");
+const express = require("express");
+
+const app = express();
+const PORT = "4000";
+
+app.listen(PORT, () => {
+  console.logt(`server sunning at por0t : ${PORT}`);
+});
+app.listen("");
